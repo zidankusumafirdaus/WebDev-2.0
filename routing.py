3d7b1c3f384a34c027105471cab47b2f754cf6c7
unknown <alhaqyrobbi@gmail.com>
2024-08-12 19:56:15 +0700

    p

--- routing.py
+++ routing.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, session
 from models import User, create_tables
 from config import Config
 from werkzeug.security import check_password_hash
+from otp import sendotp, codeotp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,7 +15,7 @@ def setup_database():
     
 @app.route("/")
 def web_application():
-    return render_template("index.html")
+    return render_template("web_application/index.html")
 
 @app.route("/register", methods = ["GET","POST"])
 def Register():
@@ -28,13 +29,45 @@ def Register():
         if user_terpakai:
             return render_template("register & otp/register.html", pesan = "Username atau Email Sudah Terpakai.")
         User.create(username, password, email)
-        return redirect("/login")
+        session['email'] = email
+        return redirect("/otp")
     return render_template("register & otp/register.html")
+
+@app.route("/otp", methods=["GET", "POST"])
+def otp():
+    if request.method == "POST":
+        input_otp = request.form.get("otp")
+        if input_otp == session.get('otp'):
+            return redirect("/otp_sukses")
+        else:
+            return render_template("register & otp/otp.html", pesan="Invalid cuy")
+
+    otp_code = codeotp()
+    session['otp'] = otp_code
+    sendotp(otp_code)
+    return render_template("register & otp/otp.html")
+
+@app.route("/otp_sukses")
+def otp_sukses():
+    return render_template("register & otp/otp_sukses.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        berhasil = User.get(username) and User.get(password)
+        if berhasil :
+            return redirect("/dashboard")
+        else :
+            return render_template('register & otp/login.html', error = 'username atau password salah')
+
     return render_template("register & otp/login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard/dash.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
