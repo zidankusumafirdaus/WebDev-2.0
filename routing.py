668ba7464a5e162c7b182488ecdeb332328f587c
unknown <raihanrizky1308@gmail.com>
2024-08-22 11:32:56 +0700

    barcode

--- routing.py
+++ routing.py
@@ -3,6 +3,9 @@ from models import User, create_tables, Admin
 from config import Config
 from werkzeug.security import check_password_hash
 from otp import sendotp, codeotp
+import qrcode
+from io import BytesIO
+from base64 import b64encode
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -124,6 +127,21 @@ def logout():
 def dashboardadmin():
     return render_template("dashboard/dashadmin.html")
 
+@app.route('/barcode')
+def barcode():
+    return render_template('barcode/barcode.html')
+
+@app.route('/generateQR', methods=['POST'])
+def generateQR():
+    memory = BytesIO()
+    data = request.form.get('link')
+    img = qrcode.make(data)
+    img.save(memory)
+    memory.seek(0)
+    
+    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode('ascii')
+    
+    return render_template('barcode/barcode.html', data=base64_img)
 
 if __name__ == "__main__":
     app.run(debug=True)
