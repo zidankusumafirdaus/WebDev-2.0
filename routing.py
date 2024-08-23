aa6e0a09350e656de48fec8c3a8f8733155b2441
unknown <raihanrizky1308@gmail.com>
2024-08-23 19:06:11 +0700

    kelupaan kodenya

--- routing.py
+++ routing.py
@@ -4,6 +4,9 @@ from config import Config
 from werkzeug.security import check_password_hash
 from otp import sendotp, codeotp
 from register import regis_admin
+import qrcode
+from io import BytesIO
+from base64 import b64encode
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -107,6 +110,22 @@ def logout():
 @app.route("/dashboardadmin")
 def dashboardadmin():
     return render_template("dashadmin.html")
+
+@app.route('/barcode')
+def barcode():
+    return render_template('barcode/barcode.html')
+
+@app.route('/generateQR', methods=['POST'])
+def generateQR():
+    memory = BytesIO()
+    data = request.form.get('link')
+    img = qrcode.make(data)
+    img.save(memory)
+    memory.seek(0)
+    
+    base64_img = "data:image/png;base64," + b64encode(memory.getvalue()).decode('ascii')
+    
+    return render_template('barcode/barcode.html', data=base64_img)
 
 
 if __name__ == "__main__":
