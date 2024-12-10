7d708c960a0447abe8d894ad98ef67d7dbee99fb
unknown <darutsyafiq@gmail.com>
2024-12-10 14:50:51 +0700

    main

---
+++ crud.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Model Database
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+# Route Home
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('crud1.html', items=items)
+
+# Route Tambah Data
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        new_item = Item(name=name, description=description)
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('crud2.html')
+
+# Route Edit Data
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    item = Item.query.get_or_404(id)
+    if request.method == 'POST':
+        item.name = request.form['name']
+        item.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('crud3.html', item=item)
+
+# Route Hapus Data
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    item = Item.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
