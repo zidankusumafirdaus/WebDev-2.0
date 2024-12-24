d7d5657b871f1533c69635f7a492ba4075fa85d2
M Zidan Kusuma Firdaus <zidankusumafirdaus.school@gmail.com>
2024-12-24 21:34:09 +0700

    QR done but not perfect

--- apps/qr/qr.py
+++ apps/qr/qr.py
@@ -1,27 +1,60 @@
 import os
+import json
 import qrcode
 import hashlib
 from flask import Flask, request, render_template, Blueprint, send_from_directory
 
 qr = Blueprint("qr", __name__, static_folder="static", template_folder="templates")
 
-FolUploads = './apps/qr/uploads'
-os.makedirs(FolUploads, exist_ok=True)
+FolderUploads = './apps/qr/uploads'
+os.makedirs(FolderUploads, exist_ok=True)
 
 @qr.route('/barcode', methods=['GET', 'POST'])
 def barcode():
     if request.method == 'POST':
-        link = request.form['link']
-        QRGenerate = qrcode.make(link)
+        if 'link' in request.form:
+            # QR Code For URL
+            link = request.form['link']
+            QRGenerate = qrcode.make(link)
+            
+            safe_link = hashlib.md5(link.encode()).hexdigest() 
+            qrSave = os.path.join(FolderUploads, f"{safe_link}.png")
+            QRGenerate.save(qrSave)
+            return render_template('qr.html', data=f"{safe_link}.png", countries=countries)
         
-        safe_link = hashlib.md5(link.encode()).hexdigest() 
-        qrSave = os.path.join(FolUploads, f"{safe_link}.png")
+        elif 'WANumber' in request.form and 'WAText' in request.form:
+            # QR Code For WhatsApp
+            country_code = request.form.get('CodeCountry')
+            WANumber = request.form['WANumber']
+            WAMessage = request.form['WAText']
 
-        QRGenerate.save(qrSave)
-        
-        return render_template('qr.html', data=f"{safe_link}.png")
-    return render_template('qr.html', data=None)
+            WhatsAppURL = f"https://wa.me/{country_code}{WANumber}?text={WAMessage}"
+            QRGenerate = qrcode.make(WhatsAppURL)
+            
+            safe_link = hashlib.md5(WhatsAppURL.encode()).hexdigest()
+            qrSave = os.path.join(FolderUploads, f"{safe_link}.png")
+            QRGenerate.save(qrSave)
+            return render_template('qr.html', data=f"{safe_link}.png", countries=countries)
+
+        elif 'GmailAddress' in request.form and 'GmailSubject' in request.form and 'GmailText' in request.form:
+            # QR Code For Gmail
+            GmailAddress = request.form['GmailAddress']
+            GmailSubject = request.form['GmailSubject']
+            GmailText = request.form['GmailText']
+
+            GmailURL = f"MATMSG:TO:{GmailAddress};SUB:{GmailSubject};BODY:{GmailText}"
+            QRGenerate = qrcode.make(GmailURL)
+
+            safe_link = hashlib.md5(GmailURL.encode()).hexdigest()
+            qrSave = os.path.join(FolderUploads, f"{safe_link}.png")
+            QRGenerate.save(qrSave)
+            return render_template('qr.html', data=f"{safe_link}.png", countries=countries)
+    return render_template('qr.html', data=None, countries=countries)
 
 @qr.route('/uploads/<path:filename>')
 def uploaded_file(filename):
-    return send_from_directory(FolUploads, filename)
+    return send_from_directory(FolderUploads, filename)
+
+# Load country codes from JSON file
+with open('apps/qr/CountryCodes.json') as f:
+    countries = json.load(f)
