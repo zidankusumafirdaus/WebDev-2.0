113163d17905d94643d7661d76c8aee4f5a1aa01
unknown <alhaqyrobbi@gmail.com>
2025-01-03 21:37:49 +0700

    Logout done || Memperbaiki path static superuser

--- apps/admin/admin.py
+++ apps/admin/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect,url_for, make_response
 from auth import login_required, role_required
 
 admin_bp = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
@@ -8,5 +8,18 @@ admin_bp = Blueprint('admin', __name__, template_folder='templates', static_fold
 @role_required('admin','superadmin') #penentu role
 def dashboardsuper():
     return render_template("dashadmin.html")
+
+
+@admin_bp.route('/logout')
+def admin_logout():
+    """Logout admin, clear session, and redirect to login page."""
+    session.clear() 
+    response = make_response(redirect(url_for('login.login')))
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    
+    return response
+
 
 # Ndisor iki tambah ono fitur gwe dashboard e admin
